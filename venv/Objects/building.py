a8f31f108381a44d774a6b6faf1b1c1cfa4f612c
dlschon <dlschon@ou.edu>
2018-03-03 13:56:43 -0600

    ignored .idea folder

---
+++ venv/Objects/building.py
@@ -0,0 +1,4 @@
+class Building:
+    def __init__(self):
+        self.level = 1
+        self.monthly_cost = 0
--- venv/Objects/university.py
+++ venv/Objects/university.py
@@ -1,8 +1,8 @@
-class university:
+class University:
     def __init__(self):
         self.money = 0
         self.reputation = 0
-        self.sudentSatisfaction = 0
-        self.graduationRate = 0
+        self.sudent_satisfaction = 0
+        self.graduation_rate = 0
         self.opportunity = 0
         self.tuition = 0
