888c6beb000970b9ab28791c8c3246a5b4ebe95e
Michael Fedell <s.michaelfedell@gmail.com>
2018-03-04 05:26:22 -0600

    add moneyinfo

--- venv/Objects/university.py
+++ venv/Objects/university.py
@@ -4,12 +4,32 @@ class University:
         self.grad_rate = .6
         self.reputation = 0
         self.sudent_satisfaction = 0
-        self.graduation_rate = 0
         self.opportunity = 0
         self.tuition = 0
+        self.target_gpa = 2.5
+        self.enrollment = 10
+        self.max_enrollment = 50
 
         self.teachers = []
         self.students = []
         self.programs = []
         self.buildings = []
         self.finances = []
+
+    def calcRevenue(self):
+        revenue = []
+        total = 0
+        for finance in self.finances:
+            if finance.value >= 0:
+                revenue.append(finance)
+                total += finance.value
+        return total, revenue
+
+    def calcExpense(self):
+        expense = []
+        total = 0
+        for finance in self.finances:
+            if finance.value < 0:
+                expense.append(finance)
+                total += finance.value
+        return total, expense
