bc93ada01df9c52caf3dbfa71c3960083d1b7952
Robert Monaco <robert@Roberts-MacBook-Pro.local>
2018-03-04 04:43:42 -0600

    stuff

--- venv/admissions.py
+++ venv/admissions.py
@@ -3,13 +3,17 @@ from Objects.student import Student
 from Objects.person import Person
 import random
 
-def admit_students(students):
-	rep = global_vars.University().reputation
-	grad_rates = global_vars.University().graduation_rates
-	opp = global_vars.University().oppurtunity
-	tuit = global_vars.University().tuition
-	programs = global_vars.University().programs
-	max_pop = global_vars.University().capacity
+def first_students():
+	for i in range(0,10):
+		global_vars.university.students.append(Student(0, random.randint(0,1), random.random(),random.randrange(2.3,4.0,0.1)))
+
+def admit_students():
+	rep = global_vars.university.reputation
+	grad_rates = global_vars.university.graduation_rates
+	opp = global_vars.university.oppurtunity
+	tuit = global_vars.university.tuition
+	programs = global_vars.university.programs
+	max_pop = global_vars.university.capacity
 
 	app_amt = 20
 	app_amt += (30 * grad_rates)
@@ -52,13 +56,13 @@ def admit_students(students):
 	accept = [] 
 	for p in range(population):
 		p = Student(0, random.randint(0,1), random.random(),random.randrange(2.0,4.0,0.1))
-		if(p.gpa >  global_var.University().accept_gpa):
+		if(p.gpa >  global_var.university.accept_gpa):
 			accept.append(p)
-	curr_students = global_var.University().students
+	curr_students = global_var.university.students
 	
 	#keep acceptances under capacity
 	while(len(curr_students) + len(accept) > max_pop):
 		accept.pop()
 	
 	#add in the new students
-	curr_students.append(accept)	
+	global_var.university.students.append(accept)	
