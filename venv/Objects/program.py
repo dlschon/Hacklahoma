7a17d216d4291313ec45704d8dec33acf1d1e2df
Robert Monaco <robert@Roberts-MacBook-Pro.local>
2018-03-04 05:51:51 -0600

    Nice

--- venv/Objects/program.py
+++ venv/Objects/program.py
@@ -1,3 +1,6 @@
+import global_var
+from Objects.finance import Finance
+
 class Program:
   def __init__(self):
     self.amountInvested = 0
@@ -10,14 +13,22 @@ class Program:
 #union programs
 class AlumniFoundation(Program):
   def __init__(self):
+	self.desc = "Foundation for Alumni to give back through"
+	self.title = "Alumni Foundation"
 	self.unlocked = false
 	self.cost = 1000
   
   def impact(self):
-	global_var.university.money += 1000
+	f = Finance()
+	f.desciption = "The Alumni Foundation gives back"
+	f.value = 1000
+	global_var.university.finances.append(f)
+	
     
 class CareerServices(Program):
   def __init__(self):
+	self.title = "Career Services"
+	self.desc = "An office to assist student in finding careers after college"
 	self.unlocked = false
 	self.cost = 1000
   
@@ -28,6 +39,10 @@ class CareerServices(Program):
 #library programs
 class Scholarships(Program):
   def __init__(self):
+	self.title = "Scholarships"
+	self.desc = "Money given to achieving students by the university for tuition"
+	self.unlocked = false
+	self.cost = 1000
 
   def impact(self):
 	for s in global_var.university.students:
@@ -35,6 +50,10 @@ class Scholarships(Program):
 
 class FinancialAid(Program):
   def __init__(self):
+	self.title = "Financial Aid"
+	self.desc = "Money given to students with financial need by the university for tuition"
+	self.unlocked = false
+	self.cost = 1000
 
   def impact(self):
 	for s in global_var.university.students:
@@ -42,7 +61,10 @@ class FinancialAid(Program):
 
 class StudyAbroad(Program):
  def __init__(self):
-
+	self.title = "Study Abroad Program"
+	self.desc = "A program for students to complete courses for credit while abroad"
+	self.unlocked = false
+	self.cost = 1000
  def impact(self):
 	for s in global_var.university.students:
 		s.productivity += 0.5
@@ -50,6 +72,10 @@ class StudyAbroad(Program):
 
 class AcademicAdvisor(Program):
  def __init__(self):
+	self.title = "Academic Advisors"
+	self.desc = "Advisors to assist students in picking out classes"
+	self.unlocked = false
+	self.cost = 1000
 
  def impact(self):
 	for s in global_var.university.students:
@@ -59,14 +85,25 @@ class AcademicAdvisor(Program):
 #stadium program
 class Rivalry(Program):
  def __init__(self):
+	self.title = "Strike up a rivalry"
+	self.desc = "Another local university believes their athletics team is superior. Prove them wrong"
+	self.unlocked = false
+	self.cost = 1000
 
  def impact(self):
-	global_var.university.money += 1000
+	f = Finance()
+	f.description = "Heated football rivalry sells tickets"
+	f.value = 1000
+	global_var.university.finances.append(f)
 	for s in global_var.university.students:
 		s.morale += 0.5
 
 class HeadCoach(Program):
  def __init__(self):
+	self.title = "Hire a head coach"
+	self.desc = "Every good team has a great leader. Hiring one will strike up the fans"
+	self.unlocked = false
+	self.cost = 1000
 
  def impact(self):
 	for s in global_var.university.students:
@@ -74,34 +111,61 @@ class HeadCoach(Program):
 
 class BetterBranding(Program):
  def __init__(self):
+	self.title = "Unify the athletic brand"
+	self.desc = "Sell merchandise of your universities' team"
+	self.unlocked = false
+	self.cost = 1000
 
  def impact(self):
-	global_var.unviersity.money += 1000
+	f = Finance()
+	f.description = "Fans love buying clothing with your athletics brand"
+	f.value = 1000
+	global_var.university.finances.append(f)
 	for s in global_var.university.students:
 		s.morale += 0.5
 
 #research lab programs
 class Groundbreaking(Program):
  def __init__(self):
+	self.title = "Groundbreaking Research"
+	self.desc = "A professor discovers something great"
+	self.unlocked = false
+	self.cost = 1000
 
  def impact(self):
-	global_var.university.money += 1000
+	f = Finance()
+	f.description = "New groundbreaking research brings in new funding"
+	f.value = 1000
+	global_var.university.finances.append(f)
 	for t in global_var.university.teachers:
 		t.research += 0.5
 
 class Collaborative(Program):
  def __init__(self):
+	self.title = "Collaborative Research"
+	self.desc = "Professors collaborate alongside other universities"
+	self.unlocked = false
+	self.cost = 1000
 
  def impact(self):
-	global_var.university.money += 1000
+	f = Finance()
+	f.description = "Your professors start to collaborate with other universities and bring in new funding"
+	f.value = 1000
+	global_var.university.finances.append(f)
 	for t in global_var.university.teachers:
 		t.research += 0.5
 
 class UndergradResearch(Program):
  def __init__(self):
-
+	self.title = "Undergraduate Research"
+	self.desc = "Implement a new undergraduate research program"
+	self.unlocked = false
+	self.cost = 1000
  def impact(self):
-	global_var.university.money += 1000
+	f = Finance()
+	f.description = "Undergrad assitence boost research and bring in new grants"
+	f.value = 1000
+	global_var.university.finances.append(f)
 	for t in global_var.university.teachers:
 		t.research += 0.5
 	for s in global_var.university.students:
@@ -110,19 +174,63 @@ class UndergradResearch(Program):
 #dining hall
 class Buffet(Program):
  def __init__(self):
+	self.title = "Buffet Menu"
+	self.desc = "All you can eat"
+	self.unlocked = false
+	self.cost = 1000
+
+ def impact(self):
+	for s in global_var.university.students:
+		s.morale += 0.5
 
 class Freshness(Program):
  def __init__(self):
+	self.title = "Fresh Ingredients"
+	self.desc = "Fresher food for fresher folks"
+	self.unlocked = false
+	self.cost = 1000
+
+ def impact(self):
+	for s in global_var.university.students:
+		s.morale += 0.5
 
 class MoreOptions(Program):
  def __init__(self):
-
+	self.title = "More Dining Options"
+	self.desc = "Variety is the spice of life"
+	self.unlocked = false
+	self.cost = 1000
+ def impact(self):
+	for s in global_var.university.students:
+		s.morale += 0.5
 #housing
 class AcademicHalls(Program):
  def __init__(self):
+	self.unlocked = false
+	self.cost = 1000
+
+ def impact(self):
+	for s in global_var.university.students:
+		s.morale += 0.5
 
 class CoedHousing(Program):
  def __init__(self):
+	self.title = "Coed Housing"
+	self.desc = "Allow for intermingling"
+	self.unlocked = false
+	self.cost = 1000
+
+ def impact(self):
+	for s in global_var.university.students:
+		s.morale += 0.5
 
 class ResidentPrograms(Program):
  def __init__(self):
+	self.title = "Housing Center Programs"
+	self.desc = "Put on events for the students within housing"
+	self.unlocked = false
+	self.cost =1000
+
+ def impact(self):
+	for s in global_var.university.students:
+		s.morale += 0.5
