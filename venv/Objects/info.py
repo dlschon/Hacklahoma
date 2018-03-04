722ff3d9ac6a607337beb2ef729731b18f3379bd
Michael Fedell <s.michaelfedell@gmail.com>
2018-03-04 03:56:39 -0600

    changed icons and finished student-info

--- venv/Objects/info.py
+++ venv/Objects/info.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from building import Building
 
 class InfoPane:
@@ -62,8 +63,38 @@ class StudentInfo(InfoPane):
         student_info_frame = Tk()
         student_info_frame.title(self.title)
         lbl = Label(text='Target GPA')
-        lbl.grid(column=1, row=1)
-
+        lbl.grid(column=1, row=1, columnspan=2, pady=(10,0))
+        slider = Scale(from_=2.0, to=4.0, orient=HORIZONTAL, resolution=0.1, length=400)
+        # Arbitrarily set to 2.8, need to add set(curr_val)
+        slider.set(2.8)
+        slider.grid(column=1, row=2, columnspan=2, pady=(0,15))
+        lbl = Label(text='Current Enrollment: ', anchor='w')
+        lbl.grid(column=1, row=3)
+        # Arbitrarily set to 50, need to set(curr_val)
+        lbl = Label(text=50, anchor='e')
+        lbl.grid(column=2, row=3)
+        lbl = Label(text='Max Enrollment: ', anchor='w')
+        lbl.grid(column=1, row=4)
+        # Arbitrarily set to 150, need to set(curr_val)
+        lbl = Label(text=150, anchor='e')
+        lbl.grid(column=2, row=4)
+        # Arbitrarily set to 50/150, need to set(curr_val)
+        pbar = ttk.Progressbar(orient=HORIZONTAL, length=300, mode='determinate', value=(50/150*100))
+        pbar.grid(column=1, row=5, columnspan=2, pady=(0,15))
+        lbl = Label(text='Average GPA: ')
+        lbl.grid(column=1, row=6, pady=15)
+        lbl = Label(text=2.3)
+        lbl.grid(column=2, row=6, pady=15)
+        lbl = Label(text='Graduation Rate: ')
+        lbl.grid(column=1, row=7, pady=15)
+        lbl = Label(text='70%')
+        lbl.grid(column=2, row=7, pady=15)
+        lbl = Label(text='Average Morale: ')
+        lbl.grid(column=1, row=8, pady=(15,0))
+        lbl = Label(text='78%')
+        lbl.grid(column=2, row=8, pady=(15,0))
+        pbar = ttk.Progressbar(orient=HORIZONTAL, length=300, mode='determinate', value=(78))
+        pbar.grid(column=1, row=9, columnspan=2, pady=(0,15))
 
         student_info_frame.mainloop()
 
