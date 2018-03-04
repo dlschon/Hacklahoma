db495ad503d31b8137d69f49fde7cc084e200f47
Robert Monaco <robert@Roberts-MacBook-Pro.local>
2018-03-04 06:43:34 -0600

    Meme

--- venv/Objects/building.py
+++ venv/Objects/building.py
@@ -1,15 +1,16 @@
 class Building:
   def __init__(self):
-    self.level = 1
-    self.impact = None
-    self.monthlyCost = 0
-    self.constructionCost = 0
+    self.level = 0
+    self.cost = 0
+    self.upgradeFee = 0
     self.capacity = 0
     self.maxLevel = 1
     self.constructionTime = 6
     self.effects = 'The effects of the building'
     self.type = None
     self.sprite = None
+    self.programs = []
+    self.num_programs = 0
 
   #def __init__(self, name, type, level, impact, monthlyCost, constructionCost, capacity, maxLevel):
   #  self.name = name
@@ -21,3 +22,12 @@ class Building:
   #  self.capacity = capacity
   #  self.maxLevel = maxLevel
   #  self.sprite = None
+
+  def upgrade(self):
+    if(global_var.university.buy(self.upgrade_cost)):
+     self.level += 1
+     self.upgrade_cost += 5000 * self.level
+
+  def add_program(self,program):	
+    if(global_var.university.buy(program.cost)):
+     self.programs.append(program)
--- venv/Objects/buildings/dininghall.py
+++ venv/Objects/buildings/dininghall.py
@@ -1,8 +1,12 @@
 from Objects.building import Building
 from Objects.sprite import Sprite
+from program import *
+
 class DiningHall(Building):
   def __init__(self):
     super().__init__()
-    self.constructionCost = 100
+    self.cost = 10000
+    self.upgrade_cost = 10000
     self.name = "Dining Hall"
     self.sprite = Sprite('Resources/dininghall', (0,0), (10,10))
+
