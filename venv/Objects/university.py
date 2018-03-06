41d94ed733d612c2aad9f4d656e2261819906204
dlschon <dlschon@ou.edu>
2018-03-06 00:09:13 -0600

    balancing

--- venv/Objects/university.py
+++ venv/Objects/university.py
@@ -3,7 +3,7 @@ from Objects.finance import Finance
 
 class University:
     def __init__(self):
-        self.money = 2000000
+        self.money = 20000
         self.grad_rate = .6
         self.reputation = 0
         self.sudent_satisfaction = 0
